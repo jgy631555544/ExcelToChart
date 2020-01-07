62a6633c2f82d4acd5cec378c4e2cd958218768a
jinguanyu <jinguanyu@foresee.com>
2020-01-07 17:25:20 +0800

    注册全局事件

--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App/index';
-window.xxzListener = {
-    xxzEvent:{},
-}
-window.xxzListener.on = (key,callback) => {
-    xxzListener.xxzEvent[key] = callback
-}
-window.xxzListener.emit = (key,data) => {
-    xxzListener.xxzEvent[key](data)
-}
+
 ReactDOM.render(<App />, document.getElementById('root'));
